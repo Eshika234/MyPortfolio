75647d95e69fbf679101b06cb74d3a67e2f6867d
Eshika Singha <98522796+Eshika234@users.noreply.github.com>
2022-07-09 14:10:03 +0530

    Update ScrollService.js

--- client/src/Utilities/ScrollService.js
+++ client/src/Utilities/ScrollService.js
@@ -12,18 +12,7 @@ export default class ScrollService{
     window.addEventListener("scroll", this.checkCurrentScreenUnderViewport);
   }
 
-  scrollToHireMe = () => {
-    let contactMeScreen = document.getElementById("ContactMe");
-    if (!contactMeScreen) return;
-
-    contactMeScreen.scrollIntoView({ behavior: "smooth" });
-  };
-  scrollToHome = () => {
-    let homeScreen = document.getElementById("Home");
-    if (!homeScreen) return;
-
-    homeScreen.scrollIntoView({ behavior: "smooth" });
-  };
+  
   isElementInView = (elem, type) => {
     let rec = elem.getBoundingClientRect();
     let elementTop = rec.top;
