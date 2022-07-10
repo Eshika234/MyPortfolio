8966dd10d2b1b1773e5d9cc94c1996234fb720e1
Eshika Singha <98522796+Eshika234@users.noreply.github.com>
2022-07-10 17:11:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,6 +22,6 @@ if (process.env.NODE_ENV === "production") {
   );
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, console.log(`server listening on port ${port}`));
