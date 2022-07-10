4e2b15eb02a0767052f1141229f3f2716d4dc752
Eshika Singha <98522796+Eshika234@users.noreply.github.com>
2022-07-10 16:49:18 +0530

    Create Footer.js

---
+++ client/src/PortfolioContainer/Footer/Footer.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './Footer.css'
+import { faLinkedin, faGithub, faInstagram } from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const Footer = () => {
+    /* SOCIALS STATIC DATA */
+    let socialDetails = [{ icon: faLinkedin, link: 'https://www.linkedin.com/in/eshikasingha/' },
+                        //   { icon: faGithub, link: 'https://github.com/Eshika234' },
+                          { icon: faInstagram, link: 'https://www.instagram.com/e_s_h_i___/' } ]
+
+    /* GET CURRENT YEAR */
+    let currentYear = new Date().getFullYear();
+
+    /* RENDER SOCIAL ICONS */
+    const getSocials = () => {
+        return (socialDetails.map((social,index) => (
+            <a rel="noopener noreferrer" key={index} href={social.link} target="_blank" >
+                <FontAwesomeIcon className="social-icon" icon={social.icon} />
+            </a>
+        )));
+    }
+
+    return (
+        <div className="main-footer-container">
+            <div className="main-footer-content">
+                <div className="main-footer-message">
+                    <span> All rights reserved © Ehizeex { currentYear } </span>
+                </div>
+                <div className="main-footer-socials">
+                    { getSocials() }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
